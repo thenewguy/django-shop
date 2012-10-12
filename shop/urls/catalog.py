6d2878e0bd73c676a3300fc5914805d948957fb8
gordon <wgordonw1@gmail.com>
2012-10-12 18:01:05 -0400

    add a ProductListView for the product list page that only shows active products

--- shop/urls/catalog.py
+++ shop/urls/catalog.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 
 from shop.views import ShopListView
-from shop.views.product import ProductDetailView
+from shop.views.product import ProductListView, ProductDetailView
 from shop.models.productmodel import Product
 
 
 urlpatterns = patterns('',
     url(r'^$',
-        ShopListView.as_view(model=Product),
+        ProductListView.as_view(model=Product),
         name='product_list'
         ),
     url(r'^(?P<slug>[0-9A-Za-z-_.//]+)/$',
